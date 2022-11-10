0b5b3f5a8a6ab303335ca986497901df277ea37d
neilloftin <117868483+neilloftin@users.noreply.github.com>
2022-11-10 11:17:05 -0500

    Add files via upload

--- ConsoleApp1/IntSumSummer.cs
+++ ConsoleApp1/IntSumSummer.cs
@@ -1,49 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace IntSumSummerApp
 {
     internal class IntSumSummer
     {
         //method inspired from https://stackoverflow.com/a/39342619
         private static void GetCombinations<T>(List<T> set, List<List<T>> result)
         {
+            result.Add(set);// below loop doesn't add original set as a valid combination of itself
             for (int i = 0; i < set.Count; i++)
             {
                 List<T> temp = new List<T>(set.Where((s, index) => index != i));
-
                 if (temp.Count > 0 && !result.Where(l => l.Count == temp.Count).Any(l => l.SequenceEqual(temp)))
                 {
                     result.Add(temp);
-
                     GetCombinations<T>(temp, result);
                 }
             }
         }
-        private List<List<int>> getSets(List<int> numList)
-        {
-            List<List<int>> sets = new List<List<int>>();
-
-            if (numList.Count <= 1)
-            {
-                return new List<List<int>>();
-            }
-            List<int> baseSet = new List<int>();
-            baseSet.AddRange(numList);
-            sets.Add(baseSet);// ie, add {1,2,3,4} if we're passed {1,2,3,4} because GetCombinations doesn't return the base set
-            GetCombinations<int>(numList, sets);
-            
-            return sets;
-        }
-
         public int GetSum(List<int> numList)
         {
-            var sets = getSets(numList);
+            var sets = new List<List<int>>();
+            GetCombinations<int>(numList, sets);
             List<int> setSums = new List<int>();
-
             foreach (var set in sets)
             {
                 var distinctSet = set.Distinct();
@@ -55,7 +32,6 @@ namespace ConsoleApp1
                     }
                 }
             }
-
             return setSums.Sum();
         }
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-using ConsoleApp1;
-
+using IntSumSummerApp;
 
 IntSumSummer summer = new IntSumSummer();
 
